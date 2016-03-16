068768222967cd5b18921d0408999025dd6553a5
Chanon Olley <olley102@gmail.com>
2016-03-16 21:08:00 +0000

    Instantiate platforms
    Platforms are instantiated when ball moves forward

--- Assets/Scripts/PlatformGen.cs
+++ Assets/Scripts/PlatformGen.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class PlatformGen : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float zInterval = 1f;
+    public float zDistance = 100f;
+    public GameObject platform1;    // make sure platforms are set to the correct prefabs in Inspector
+    public GameObject platform2;
+    public float zReached = 0f;
+
+    void Start() {
+        for (float a = 2f; a < zDistance+1f; a += 2) {
+            Instantiate(platform1, new Vector3(0, 0, a), Quaternion.identity);
+        }
+        for (float b = 3f; b < zDistance+1f; b += 2) {
+            Instantiate(platform2, new Vector3(0, 0, b), Quaternion.identity);
+        }
+    }
+
+    void FixedUpdate() {
+
+        if (Mathf.Round(transform.position.z) > zReached) {
+            zReached = Mathf.Round(transform.position.z);
+            if (zReached % 2 == 0) {
+                Instantiate(platform1, new Vector3(0, 0, zReached + zDistance), Quaternion.identity);
+            }
+            else {
+                Instantiate(platform2, new Vector3(0, 0, zReached + zDistance), Quaternion.identity);
+            }
+        }
+    }
 }
