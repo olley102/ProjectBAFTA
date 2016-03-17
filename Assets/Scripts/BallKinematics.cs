f15d153bf78e6d8597a7c57fc6f5f5a31c1c5ff0
Binary <12lkeenan@judd.kent.sch.uk>
2016-03-17 20:51:31 +0000

    Camera Update
    An update to the camera that takes out bugginess when the ball is
    falling, and going up.

--- Assets/Scripts/BallKinematics.cs
+++ Assets/Scripts/BallKinematics.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class BallKinematics : MonoBehaviour {
 
+	public static BallKinematics Kinematics;
     public float v0;    // initial velocity for start and each bounce
     public Rigidbody rb;
+	public float bounceHeight; // For the camera to move up.
 
 	void Start () {
         rb = GetComponent<Rigidbody>();
+		bounceHeight = 0;
+		Kinematics = this;
 	}
 	
 	void Update () {
@@ -17,6 +21,7 @@ public class BallKinematics : MonoBehaviour {
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag == "Platform") {
             rb.velocity = new Vector3(rb.velocity.x, v0, rb.velocity.z);
+			bounceHeight = transform.position.y;
         }
     }
 }
