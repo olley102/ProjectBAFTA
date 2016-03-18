504022996d549de3bba44d0d8bd01765b1a1ae5a
Binary <12lkeenan@judd.kent.sch.uk>
2016-03-18 16:43:41 +0000

    Responsive Camera
    Quick update that makes camera slightly more responsive when the ball is
    going up.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour {
     private float newY; // The Y that the camera will lerping to
 	private float t; // Keeps track of time for lerp 
 	public float followSpeed; // Speed of the camera's lerp on Y
+
     void Awake() {
         player = GameObject.Find("BlueBall").transform;
         relCameraPos = transform.position - player.position;
@@ -16,23 +17,23 @@ public class CameraController : MonoBehaviour {
     }
 
     void FixedUpdate() {
-		if ((transform.position.y - player.position.y < -2.5 + relCameraPos.y) && (player.position.y + relCameraPos.y - 2.5f > newY )) { // If it goes Below the bottom of the bounce
-            newY = player.position.y + relCameraPos.y - 2.5f;
+
+		if (BallKinematics.Kinematics.bounceHeight + relCameraPos.y > newY) { // If the ball bounces higher than expected, indicating that it has moved up:
+			newY = BallKinematics.Kinematics.bounceHeight + relCameraPos.y;
+			BallKinematics.Kinematics.bounceHeight = -1024;
 			t = 0;
-        }
+		}
 
 		if ((transform.position.y - player.position.y > relCameraPos.y) && (player.position.y + relCameraPos.y < newY )) { // If it goes above the top of the bounce
 			newY = player.position.y + relCameraPos.y;
 			t = 0;
 		} 
 
-		//if (BallKinematics.Kinematics.bounceHeight + relCameraPos.y > newY) { // If the ball bounces higher than expected, indicating that it has moved up:
-		//	newY = BallKinematics.Kinematics.bounceHeight + relCameraPos.y;
+		//if ((transform.position.y - player.position.y < -2.5 + relCameraPos.y) && (player.position.y + relCameraPos.y - 2.5f > newY )) { // If it goes Below the bottom of the bounce
+		//	newY = player.position.y + relCameraPos.y - 2.5f;
 		//	t = 0;
 		//}
-
-		//wip
-	
+		// OLD
 
 		t = t + Time.deltaTime * followSpeed;
 		transform.position = new Vector3(player.position.x + relCameraPos.x, Mathf.Lerp(transform.position.y, newY, t), player.position.z + relCameraPos.z);
