2465e1b15e96913562adb91a5ecc9d242c953b9e
Binary <12lkeenan@judd.kent.sch.uk>
2016-03-19 14:26:40 +0000

    Smoother Lighting
    Makes the change of lighting smoother.

--- Assets/FlipLightOnGravity.cs
+++ Assets/FlipLightOnGravity.cs
@@ -6,18 +6,29 @@ public class FlipLightOnGravity : MonoBehaviour {
     public float eulerX = 50f;
     public float eulerY = -30f;
     public float eulerZ = 0f;
+	public float lerpspeed;
+
+	private float t = 0f;
+	private Quaternion newrot;
 
 	// Use this for initialization
 	void Start () {
         transform.rotation = Quaternion.Euler(eulerX, eulerY, eulerZ);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         if ((Input.GetKeyDown(KeyCode.Space))) {
-            transform.rotation = Quaternion.Euler(eulerX * -1, eulerY * -1, eulerZ);
+            newrot = Quaternion.Euler(eulerX * -1, eulerY * -1, eulerZ);
             eulerX *= -1;
             eulerY *= -1;
+			t = 0;
         }
+		if (t < 1) {
+			t = t + Time.deltaTime * lerpspeed;
+		} else {
+			t = 1;
+		}
+		transform.rotation = Quaternion.LerpUnclamped (transform.rotation, newrot, t);
     }
 }
